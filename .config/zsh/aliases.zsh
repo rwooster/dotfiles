313d89fc5478178e7db9b949350c5a28d9475987
Ryan Wooster <ryan@rwooster.io>
2025-05-08 23:18:04 -0400

    Bring in fzf config and functions

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -10,3 +10,34 @@ alias al="nvim ~/.config/zsh/aliases.zsh"
 alias rc="nvim ~/.config/zsh/.zshrc"
 alias term="nvim ~/.config/alacritty/alacritty.toml"
 alias load="source ~/.config/zsh/.zshrc"
+
+# Useful functions
+xargs_no_run_if_empty() {
+  # On MacOS, xargs default behavior is `--no-run-if-empty`,
+  # and that parameter is not accepted. On linux, the parameter
+  # needs to be passed in.
+
+  MAYBE_NO_RUN_IF_EMPTY=""
+  if [[ "${uname_out}" == "Linux" ]]; then
+    MAYBE_NO_RUN_IF_EMPTY="--no-run-if-empty"
+  fi
+
+  xargs ${MAYBE_NO_RUN_IF_EMPTY} "$@"
+}
+
+checkout() {
+  git branch |
+    rg --invert-match '\*' |
+    cut -c 3- |
+    fzf --preview="git log {}" |
+    xargs_no_run_if_empty git checkout
+}
+alias ch="checkout"
+
+delete-branches() {
+  git branch |
+    rg --invert-match '\*' |
+    cut -c 3- |
+    fzf --multi --preview="git log {}" |
+    xargs_no_run_if_empty git branch --delete --force
+}
--- .config/zsh/integrations.zsh
+++ .config/zsh/integrations.zsh
@@ -4,3 +4,12 @@
 
 # Enable fzf
 eval "$(fzf --zsh)"
+
+# Configure fzf
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+export FZF_DEFAULT_COMMAND='fd --type file --follow --color=always --exclude bin/'
+# This lets the fd colors work. This can be slow, remove this if fzf seems to start lagging.
+export FZF_DEFAULT_OPTS="--ansi"
+export FZF_CTRL_T_COMMAND='fd --follow --strip-cwd-prefix --color=always --exclude bin/'
+export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
