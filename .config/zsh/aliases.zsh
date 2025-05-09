809fe692eadd68b6d2b097e39e5ca0dd3d0f0c8d
Ryan Wooster <ryan@rwooster.io>
2025-05-08 22:46:59 -0400

    more aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -2,8 +2,11 @@ alias v="nvim"
 alias vi="nvim"
 alias vim="nvim"
 alias ls="ls --color=auto"
+alias ll="ls -la"
 alias gitfix='git commit --amend --no-edit'
 
 # Quickly open config files
 alias al="nvim ~/.config/zsh/aliases.zsh"
 alias rc="nvim ~/.config/zsh/.zshrc"
+alias term="nvim ~/.config/alacritty/alacritty.toml"
+alias load="source ~/.config/zsh/.zshrc"
