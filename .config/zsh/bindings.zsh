441fd56d9fc0859a4fabc6c7fd19e845acb19bb5
Ryan Wooster <ryan-wooster@woven-planet.global>
2025-05-19 14:32:55 -0400

    Add update for vi style cursor indicator

--- .config/zsh/bindings.zsh
+++ .config/zsh/bindings.zsh
@@ -3,5 +3,35 @@
 ###
 
 bindkey -v
+bindkey -M viins 'jj' vi-cmd-mode
+
 bindkey '^r' history-search-backward
 #bindkey "^R" history-incremental-pattern-search-backward
+
+
+# Change cursor between insert and edit mode to be similar to vi.
+cursor_mode() {
+    cursor_block='\e[2 q'
+    cursor_beam='\e[6 q'
+
+    function zle-keymap-select {
+        if [[ ${KEYMAP} == vicmd ]] ||
+            [[ $1 = 'block' ]]; then
+            echo -ne $cursor_block
+        elif [[ ${KEYMAP} == main ]] ||
+            [[ ${KEYMAP} == viins ]] ||
+            [[ ${KEYMAP} = '' ]] ||
+            [[ $1 = 'beam' ]]; then
+            echo -ne $cursor_beam
+        fi
+    }
+
+    zle-line-init() {
+        echo -ne $cursor_beam
+    }
+
+    zle -N zle-keymap-select
+    zle -N zle-line-init
+}
+
+cursor_mode
