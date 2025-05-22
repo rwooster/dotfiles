88a0bd8510eb0fd65b78a209e32859117f3b1d2d
Ryan Wooster <ryan-wooster@woven-planet.global>
2025-05-22 15:50:27 -0400

    Tweaks to tmux config and make sure history file exists

--- .config/zsh/settings.zsh
+++ .config/zsh/settings.zsh
@@ -17,3 +17,8 @@ setopt hist_ignore_all_dups
 setopt hist_save_no_dups
 setopt hist_ignore_dups
 setopt hist_find_no_dups
+
+if [[ ! -f "${HISTFILE}" ]]; then
+    mkdir -p ${XDG_DATA_HOME}/zsh/
+    touch ${HISTFILE}
+fi
