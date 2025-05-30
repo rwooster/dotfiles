c0833c5f4e2d8e88c24712548b498a892ba363ec
Ryan Wooster <ryan@rwooster.io>
2025-05-29 23:54:14 -0400

    Add some git configuration

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -13,6 +13,32 @@ alias zrc="nvim ~/.config/zsh/.zshrc"
 alias tmc="nvim ~/.config/tmux/tmux.conf"
 alias alc="nvim ~/.config/alacritty/alacritty.toml"
 alias vrc="nvim ~/.config/nvim/init.lua"
+alias gitrc="nvim ~/.config/git/config"
+
+# Git command aliases
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
+alias gb="git branch"
+# Hide the "+" and "-" by default and rely on colors.
+alias gd="git diff --output-indicator-new=' ' --output-indicator-old=' '"
+# Add a "verbose" mode which includes the indictors if needed.
+alias gdv="git diff"
+alias gds="gd --staged"
+alias gcl="git clone"
+alias gpu="git push"
+alias gpl="git pull"
+
+# Format guide:
+# %C Set color
+# %h commit hash
+# %an author name
+# %ar commit time (relative)
+# %D refs
+# %n newline
+# %s commit message
+# TODO: Get a common definition for colors
+alias gl="git log --graph --all --pretty=format:'%C(11)%h %C(10)%an%C(8) <%ar> %C(4)%D%n%s%n'"
 
 # Reload the .zsh config file
 alias rld="source ~/.config/zsh/.zshrc"
