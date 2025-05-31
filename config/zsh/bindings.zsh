e37ce54034e4475126962d2ac4aa6852dde75304
Ryan Wooster <ryan@rwooster.io>
2025-05-31 10:52:07 -0400

    Add zsh vi mode text objects. Might want to consolidate completion config

--- config/zsh/bindings.zsh
+++ config/zsh/bindings.zsh
@@ -35,3 +35,18 @@ cursor_mode() {
 }
 
 cursor_mode
+
+
+# Add quote/bracket text objects to vi mode
+autoload -Uz select-bracketed select-quoted
+zle -N select-quoted
+zle -N select-bracketed
+for km in viopp visual; do
+  bindkey -M $km -- '-' vi-up-line-or-history
+  for c in {a,i}${(s..)^:-\'\"\`\|,./:;=+@}; do
+    bindkey -M $km $c select-quoted
+  done
+  for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+    bindkey -M $km $c select-bracketed
+  done
+done
