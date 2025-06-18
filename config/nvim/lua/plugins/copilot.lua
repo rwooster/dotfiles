2f9712ef0389eae040cb08f31d93cc5625bacf14
Ryan Wooster <ryan.wooster@woven-planet.global>
2025-06-18 12:04:14 -0400

    initial copilot config

---
+++ config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,30 @@
+-- return {
+--     "github/copilot.vim",
+--     -- disable copilot by default
+--     lazy = true,
+--     keys = { "<leader>ce" },
+--     config = function()
+--         vim.keymap.set("i", "<C-j>", 'copilot#Accept("\\<CR>")', {
+--             expr = true,
+--             replace_keycodes = false,
+--         })
+--         vim.keymap.set("n", "<leader>cd", ":Copilot disable <CR>", {})
+--         vim.keymap.set("n", "<leader>ce", ":Copilot enable <CR>", {})
+--     end,
+-- }
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      keymap = {
+        accept = "<C-j>",
+        next = "<C-]>",
+        prev = "<C-[>",
+      }
+    },
+  },
+}
