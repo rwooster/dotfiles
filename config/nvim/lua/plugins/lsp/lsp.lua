31e29a108fdb00d5d055b9a25192258f61e8c0f5
Ryan Wooster <ryan@rwooster.io>
2025-06-19 13:49:09 -0400

    Update to neovim 0.11 style LSP. Update plugins

--- config/nvim/lua/plugins/lsp/lsp.lua
+++ config/nvim/lua/plugins/lsp/lsp.lua
@@ -1,17 +1,20 @@
 return {
     -- Main LSP Configuration
     "neovim/nvim-lspconfig",
-    event = "InsertEnter",
     dependencies = {
         -- Automatically install LSPs and related tools to stdpath for Neovim
         -- Mason must be loaded before its dependents so we need to set it up here.
-        { "williamboman/mason.nvim", opts = {} },
-        "williamboman/mason-lspconfig.nvim",
+        { "mason-org/mason.nvim", opts = {} },
+
+        -- Used translate between nvim-lspconfig server names and mason.nvim
+        -- package names (e.g. lua_ls <-> lua-language-server)
+        { "mason-org/mason-lspconfig.nvim", opts = { automatic_enable = false } },
+
+        -- Used to install all required Mason tools.
         "WhoIsSethDaniel/mason-tool-installer.nvim",
 
         -- Useful status updates for LSP.
         { "j-hui/fidget.nvim", opts = {} },
-        "saghen/blink.cmp",
     },
     config = function()
         --  This function gets run when an LSP attaches to a particular buffer.
@@ -147,45 +150,19 @@ return {
             },
         })
 
-        -- LSP servers and clients are able to communicate to each other what features they support.
-        --  By default, Neovim doesn't support everything that is in the LSP specification.
-        --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-        --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-        local original_capabilities = vim.lsp.protocol.make_client_capabilities()
-        local capabilities = require("blink.cmp").get_lsp_capabilities(original_capabilities)
-
         local packages = require("plugins.lsp.mason_packages")
-
-        -- Ensure the servers and tools above are installed
-        --
-        -- To check the current status of installed tools and/or manually install
-        -- other tools, you can run
-        --    :Mason
-        --
-        -- You can press `g?` for help in this menu.
-        --
-        -- `mason` had to be setup earlier: to configure its options see the
-        -- `dependencies` table for `nvim-lspconfig` above.
-        --
-        -- You can add other tools here that you want Mason to install
-        -- for you, so that they are available from within Neovim.
         local ensure_installed = vim.tbl_keys(packages.servers or {})
         vim.list_extend(ensure_installed, packages.tools)
+
+        -- Install all required packages
         require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-        require("mason-lspconfig").setup({
-            ensure_installed = {}, -- explicitly set to an empty table (Installs populated via mason-tool-installer)
-            automatic_installation = false,
-            handlers = {
-                function(server_name)
-                    local server = packages.servers[server_name] or {}
-                    -- This handles overriding only values explicitly passed
-                    -- by the server configuration above. Useful when disabling
-                    -- certain features of an LSP (for example, turning off formatting for ts_ls)
-                    server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-                    require("lspconfig")[server_name].setup(server)
-                end,
-            },
-        })
+        -- Enable all language servers
+        for server_name, config in pairs(packages.servers) do
+            if not vim.tbl_isempty(config) then
+                vim.lsp.config(server_name, config)
+            end
+            vim.lsp.enable(server_name)
+        end
     end,
 }
